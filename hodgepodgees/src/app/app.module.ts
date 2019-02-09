7c29ec1a2152ae34e2ceafb6539f5e244a0a6a44
agtrevino <39744373+agtrevino@users.noreply.github.com>
2019-02-09 16:01:10 -0600

    updates

--- hodgepodgees/src/app/app.module.ts
+++ hodgepodgees/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductPageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
